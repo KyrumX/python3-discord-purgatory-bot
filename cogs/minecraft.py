7f43ca18da9aa902f398c3216177429a7364bca9
Aaron <kyrumx@hotmail.nl>
2021-01-22 18:45:25 +0100

    -Initial setup for Minecraft Cog, which will allow users to have a certain (limited) control of the Minecraft server like starting it up.

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -1,13 +1,38 @@
+import os
+import subprocess
+
 from discord.ext import commands
+from mcstatus import MinecraftServer
 
 
-class MinecraftCog:
+class MinecraftCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        self.ip = os.getenv("MC_SERVER_IP", None)
+        self.start_bat = os.getenv("MC_SERVER_BAT", None)
+
+        if not self.ip or not self.start_bat:
+            print("No IP or start bat file provided!")
+            self.cog_unload()
+
+        self.server = MinecraftServer.lookup(self.ip)
+
+    @commands.command()
     @commands.has_role("Administrator")
-    async def startserver(self, ctx, argument):
-        pass
+    async def startserver(self, ctx):
+        # TODO: Add time delay (e.g. command cannot be spammed!)
+        try:
+            self.server.ping()
+            server_online = True
+        except:
+            server_online = False
+
+        if server_online:
+            await ctx.send('Server seems already up and running!')
+        else:
+            subprocess.Popen([self.start_bat])
+            await ctx.send('Starting server, please wait a couple of minutes!')
 
 
 def setup(bot):
