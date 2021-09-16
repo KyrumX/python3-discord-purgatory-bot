bd2081f79a0462333129e5fd555a182ebc9e3055
Aaron <kyrumx@hotmail.nl>
2021-09-16 18:37:20 +0200

    Added pause, resume, clear, skip functions

--- cogs/youtubeplayer.py
+++ cogs/youtubeplayer.py
@@ -33,9 +33,11 @@ class YoutubePlayer(commands.Cog):
     async def stop(self, ctx):
         """Stops and disconnects the bot from voice"""
 
-        self.queue.clear()
+        await self.clear(ctx)
 
         await ctx.voice_client.disconnect()
+
+        await ctx.send("Disconnected.")
 
     @yt.command(brief='Plays the audio of a youtube video, direct link or search query',
                 description="Plays the audio of a youtube video, will accept direct link or search query")
@@ -80,18 +82,72 @@ class YoutubePlayer(commands.Cog):
                 video_url = ydl.extract_info(f"ytsearch:{search_query}", download=False)['entries'][0]['formats'][0]['url']
             except:
                 raise commands.CommandError("Invalid input provided for search.")
-            print(video_url)
             return video_url
 
     @yt.command(brief='Skip the current song',
                 description="Skips the current song and starts the next song")
-    def pause(self, ctx):
-        pass
+    async def skip(self, ctx):
+        """"
+        Stops the current song and goes to the next one, if applicable
+        """
+        if ctx.voice_client is not None:
+            voice = ctx.voice_client
+            voice.stop()
+
+            await ctx.send("Skipping...")
+            self.play_next(ctx)
+
+    @yt.command(brief='Pause the music playback',
+                description="Pauses the current music track")
+    async def pause(self, ctx):
+        """"
+        Pauses playback
+        """
+        voice = ctx.voice_client
+
+        if not voice.is_paused():
+            await ctx.send("Pausing...")
+            voice.pause()
+        else:
+            await ctx.send("Already paused.")
+
+    @yt.command(brief='Resume the music playback',
+                description="Resumes the current music track")
+    async def resume(self, ctx):
+        """"
+        Resumes playback
+        """
+        voice = ctx.voice_client
+
+        if not voice.is_paused():
+            await ctx.send("Already playing.")
+        else:
+            await ctx.send("Resuming the music...")
+            voice.resume()
+
+    @yt.command(brief='Purges the queue',
+                description="Clears the queue of songs to be played")
+    async def clear(self, ctx):
+        """"
+        Clears the queue
+        """
+        self.queue.clear()
+
+        await ctx.send("Queue cleared.")
 
     @play.before_invoke
-    async def ensure_author_voice(self, ctx):
+    @pause.before_invoke
+    @skip.before_invoke
+    @resume.before_invoke
+    @clear.before_invoke
+    async def play_back_checks(self, ctx):
+        await self.ensure_bot_active_voice(ctx)
+        await self.ensure_author_same_voice_bot(ctx)
+
+    async def ensure_bot_active_voice(self, ctx):
         """"
-        Ensures that the author is actually in a voice channel
+        Ensures the following:
+            Bot is active in a voice channel, if not connect it to the author's vc, if applicable
         """
         if ctx.voice_client is None:
             if ctx.author.voice:
@@ -99,6 +155,15 @@ class YoutubePlayer(commands.Cog):
             else:
                 await ctx.send("You are not connected to a voice channel!")
                 raise commands.CommandError("Author not connected to valid voice channel.")
+
+    async def ensure_author_same_voice_bot(self, ctx):
+        """"
+        Ensures the following:
+            Bot is connected to the same voice channel as the author
+        """
+        if not ctx.author.voice or ctx.voice_client.channel != ctx.author.voice.channel:
+            await ctx.send("You are not in the same channel as the bot.")
+            raise commands.CommandError("Bot is in a different channel than the author.")
 
 
 def setup(bot):
